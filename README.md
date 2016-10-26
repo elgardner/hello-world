92bbaff607b309b19869ffcb9a492d28ffe20988
elgardner <ella.gardner@netzero.com>
2016-10-26 13:37:26 -0500

    changes 10/26/2016

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hello-world
 First repository 
+
+
+CHANGELOG
+added helloworld.py - 10/26/16
---
+++ hello-world.py
@@ -0,0 +1,7 @@
+#hello-world.py
+#Author: Ella Gardner
+#Date: 10/26/16
+
+""" Program that tells the world 'Hello'"""
+
+print("Hello, World!")
